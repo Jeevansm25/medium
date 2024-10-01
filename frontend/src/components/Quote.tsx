45a1e535d2d87e0e96a798cd9a244af363b77466
Jeevan S M <jeevansm@Jeevans-MacBook-Air.local>
2024-10-02 00:17:02 +0530

    Added fe

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+export const Quote =()=>{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center ">
+            <div className="max-w-lg ">
+        <div className="  text-3xl font-bold">
+            "The Customer support I received was exceptional . The Support team went above and below to address mh concerns "
+        </div>
+        <div className="max-w-md  text-left font-semibold text-xl text-bold mt-4">
+            Juleis Winfeild
+        </div>
+        <div className="max-w-md  text-left font-light text-slate-400 text-md text-bold ">
+            CEO | Acme corp
+        </div>
+        </div> </div>
+    </div>
+}
